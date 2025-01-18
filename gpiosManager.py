11d29aa040d559734f4732d4939acd05e89df7f8
David Diaz <david.diaz190799@gmail.com>
2025-01-17 20:58:49 -0500

    se crea el repositorio

---
+++ gpiosManager.py
@@ -0,0 +1,24 @@
+import RPi.GPIO as GPIO
+import time
+
+class GpiosManager():
+    def __init__(self):
+        super().__init__()
+        GPIO.setmode(GPIO.BCM)
+        GPIO.setwarnings(False)
+        self.relay_1 = 3
+        self.relay_2 = 4
+        # declaracion de salidas
+        GPIO.setup(self.relay_1, GPIO.OUT)
+        GPIO.setup(self.relay_2, GPIO.OUT)
+        #inicializacion
+        GPIO.output(self.relay_1,GPIO.HIGH)
+        GPIO.output(self.relay_2,GPIO.HIGH)
+
+    def turnstileOpen(self):
+        GPIO.output(self.relay_1, GPIO.LOW)
+        GPIO.output(self.relay_1, GPIO.LOW)
+        time.sleep(1)
+        GPIO.output(self.relay_1, GPIO.HIGH)
+        GPIO.output(self.relay_1, GPIO.HIGH)
+        return "puerta general abierta"
---
+++ main.py
@@ -0,0 +1,73 @@
+import serial
+import time
+import requests
+import json
+from gpiosManager import GpiosManager
+port = "/dev/ttyACM0"
+baudrate = 9600 
+timeout = 1 
+url = "http://192.168.3.92:5000/api/qr_validator"
+headers = {
+    'Content-Type': 'application/json'
+}
+turnstile = GpiosManager()
+def make_petition(_data):
+    try:
+        response = requests.post(url, headers=headers, data=json.dumps(_data))
+        if response.status_code == 200:
+            response_data = response.json()
+            authorization = response_data.get("authorization", False)
+            if authorization:
+                print("INGRESA")
+                turnstile.turnstileOpen()
+            else:
+                print("USUARIO NO PERTENECE")
+            print("Authorization:", authorization)
+        else:
+            print("Error en la solicitud:", response.status_code)
+            print("Detalles:", response.text)  
+    except requests.RequestException as e:
+        print("Error al realizar la petición:", e)
+try:
+    with serial.Serial(port, baudrate, timeout=timeout) as ser:
+        print(f"Escuchando en {port} a {baudrate} baudios. Presiona Ctrl+C para salir.")
+        while True:
+            if ser.in_waiting: 
+                data = ser.readline().decode('utf-8', errors='ignore').strip()
+                result_list = data.split("|")
+                print({"qr debug >> "+data})
+                current_timestamp = int(time.time())
+                print("c_t >> "+str(current_timestamp))
+                if len(result_list) == 4:
+                    if current_timestamp <= int(result_list[3]):
+                        query_data = {
+                            'user_id':result_list[0],
+                            'tenant_id':result_list[1],
+                            'iat':result_list[2],
+                            'exp':result_list[3]
+                        }
+                        make_petition(query_data)
+                    else:
+                        print("CODIGO EXPIRADO")
+                        
+                elif len(result_list) == 3:
+                    if current_timestamp <= int(result_list[2]):
+                        query_data = {
+                            'user_id':result_list[0],
+                            'tenant_id':result_list[1],
+                            'exp':result_list[2]
+                        }
+                        make_petition(query_data)
+                    else:
+                        print("CODIGO EXPIRADO")
+                    pass
+                else:
+                    print("BAD QR CODE")
+
+
+
+
+except serial.SerialException as e:
+    print(f"Error al acceder al puerto {port}: {e}")
+except KeyboardInterrupt:
+    print("\nPrograma terminado por el usuario.")
